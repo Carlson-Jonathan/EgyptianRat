2944394cfe75a2492aac30d4c352e00d81244939
Jonathan Carlson <JonathanCarlson3712@Hotmail.com>
2022-06-10 20:18:02 -0500

    fixed main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@
 #include <iostream>
 #include <time.h>
 #include "initializer.h"
-#include "gameLogic.h"
+#include "gameTable.h"
+#include "gameMenu.h"
+#include "titleScreen.h"
 
 using namespace std;
 
@@ -14,13 +16,27 @@ int main() {
 
 	srand(time(NULL)); 		// For seeding the random number generator
 	Initializer globalData;
-	GameLogic gameLogic(globalData);
+	TitleScreen titleScreen (globalData);
+	GameMenu    gameMenu    (globalData);
+	GameTable   gameTable;
 
 	while(globalData.window.isOpen()) {
 		globalData.eventHandler.listen();
 		globalData.window.clear(sf::Color(0, 90, 0));
 
-		gameLogic.gameLogicLoop();
+		if(globalData.mayInitializeGameTable) {
+			globalData.mayInitializeGameTable = false;
+			gameMenu.updateNumberOfPlayersText(globalData.numberOfPlayers);
+			gameTable.construct(globalData);
+		}
+
+		// Toggle between game and menu
+		if(globalData.atTitleScreen)
+			titleScreen.titleScreenLoop();
+		else if(globalData.gameMenuIsOpen)
+			gameMenu.gameMenuLoop();
+		else
+			gameTable.gameTableLoop();
 
 		globalData.window.display();
 	} 
